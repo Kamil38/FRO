5fbe5a12918fe4b2f61a9fa2ccc7f22f518818f3
Kamil38 <55272106+Kamil38@users.noreply.github.com>
2020-01-17 12:00:52 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -10,9 +10,7 @@ function showArray() {
 
 gerechten2(0);
 function gerechten2(number) {
-	for (let i = 0; i < gerechten.length; i++) {
-		console.log(gerechten[number]);
-	}	
+	console.log(gerechten[number]);	
 }
 
 console.log(gerechten1("lasagne", "soep"));
@@ -20,13 +18,28 @@ console.log(gerechten1("lasagne", "soep"));
 let lasagne = gerechten1("lasagne", "soep");
 console.log(lasagne);
 
-function gerechten1(name) {
-	return "macaroni" + name;
+function gerechten1(name,arg2) {
+	return "macaroni" + name + arg2;
 }
 
-for (let i = 2; i <= 4; i++) {
-	console.log(gerechten[i]);
+function gerechten4() {
+	for (let i = 2; i <= 4; i++) {
+		console.log(gerechten[i]);
+	}
 }
 
 
+order('ol');
+function order(tag){
+
+let ol = document.createElement(tag);
+document.getElementById('list').appendChild(ol);
+ 
+gerechten.forEach(function(name){
+	let li = document.createElement('li');
+	ol.appendChild(li);
+	li.innerHTML += name;
+});
+
+}
 gerechten1.innerHTML = gerechten;
